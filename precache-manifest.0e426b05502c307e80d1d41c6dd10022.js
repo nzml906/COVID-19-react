df2d200ee9854d2ee7c38cfd748d2a55d09bb639
Nazmul <grey000906@gmail.com>
2020-09-25 00:35:36 +0600

    Updates

---
+++ precache-manifest.0e426b05502c307e80d1d41c6dd10022.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c380625b3fb102a5cea79b1aa8573ac",
+    "url": "/COVID-19-react/index.html"
+  },
+  {
+    "revision": "ee1559889c067a9619b3",
+    "url": "/COVID-19-react/static/css/main.8cab5f18.chunk.css"
+  },
+  {
+    "revision": "adbc43befab2b34dc893",
+    "url": "/COVID-19-react/static/js/2.9a70d7e3.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/COVID-19-react/static/js/2.9a70d7e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1559889c067a9619b3",
+    "url": "/COVID-19-react/static/js/main.36dadafd.chunk.js"
+  },
+  {
+    "revision": "f5c17864c334cfe3f041",
+    "url": "/COVID-19-react/static/js/runtime-main.4b18a894.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-react/static/media/image.d7265326.png"
+  }
+]);
